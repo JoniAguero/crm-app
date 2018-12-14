cd521fc3a2cef049d4c665123f88135c96585a74
Joni Agüero <aguerojonathanivan@gmail.com>
2018-12-14 15:06:36 -0300

    Integrando Graphql con Express

--- graphql-server/index.js
+++ graphql-server/index.js
@@ -1,11 +1,23 @@
 import express from 'express';
-import chalk from "chalk";
+import chalk from 'chalk';
+import graphqlHTTP from 'express-graphql';
+import schema from './schemas';
 
 const app = express();
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send('Hellow World!')
 })
+
+const root = {
+    hola: () => 'Hola World desde Graph Ql'
+}
+// graphqlHTTP nos permite conectar graphql con express
+app.use('/graphql', graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true
+}))
 
 const port = process.env.PORT || 8000
 
